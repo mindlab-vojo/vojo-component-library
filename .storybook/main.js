f742e257bebc82bbb0aa84380786b3ccaccbbadf
Leonardo Isso <leoisso.work@gmail.com>
2020-02-27 11:38:05 -0300

    Register knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs/register',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
