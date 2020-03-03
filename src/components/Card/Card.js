069f5ab15dacdebe2aa5d77bc1ff83331b84ce58
Leonardo Isso <leoisso.work@gmail.com>
2020-03-03 10:48:31 -0300

    Add Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = ({ image, title, description }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Card.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+}
+
+export default Card
---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import * as style from '../../style'
+
+export const Wrapper = styled.div`
+    border: 1px solid ${style.colors.greyColor};
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`
