8115e92ed553d51973c1ac789d31afd111ee0d68
Leonardo Isso <leoisso.work@gmail.com>
2020-03-11 12:06:19 -0300

    Remove pkg

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 
 export default [
   {
