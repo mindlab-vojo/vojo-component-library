0fa77451c5ee3a0d21e85b2f7576deb51f0455d4
Leonardo Isso <leoisso.work@gmail.com>
2020-03-11 11:52:45 -0300

    Add rollupconfig

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        dir: 'dist/index.js',
+        format: 'cjs',
+        banner: '/* eslint-disable */',
+      },
+    ],
+    external: [
+      'react', 
+      'react-proptypes'
+    ],
+    plugins: [
+      resolve(),
+      babel({ 
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react']
+      }),
+      commonjs({
+        namedExports: {
+          'node_modules/react-is/index.js': [
+            'isElement',
+            'isValidElementType',
+            'ForwardRef'
+          ]
+        }
+      })
+    ],
+  },
+];
