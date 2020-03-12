428d2ace31a71d71c8b0229f850c1f37559af43f
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 16:27:41 -0300

    Remove sample

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Card from './components/Card';
-import Sample from './components/Sample';
 
 import ThemeConsumer, { ThemeProvider } from './ThemeManager/ThemeProvider'
 
@@ -7,7 +6,6 @@ import * as themes from './style/themes'
 
 export {
   Card,
-  Sample,
   ThemeConsumer,
   ThemeProvider,
   themes,
