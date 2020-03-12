2fbf2e55b4da4c01959982542eb2e08a82a09217
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 15:13:23 -0300

    Add Theme Context

---
+++ src/style/themes/ThemeProvider.js
@@ -0,0 +1,45 @@
+import React, { Component, createContext } from 'react'
+import PropTypes from 'prop-types'
+
+import ThemeStore from './ThemeStore';
+
+// Provider and Consumer instances
+const { Provider, Consumer } = createContext();
+
+class ThemeProvider extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    theme: PropTypes.string
+  }
+
+  state = {
+    themeConfig: ThemeStore()
+  }
+
+  componentDidMount() {
+    if (this.props.theme) this.setTheme()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.theme !== prevProps.theme) this.setTheme()
+  }
+
+  setTheme = () => {
+    const { theme } = this.props
+    const { themeConfig } = this.state
+    const updatedTheme = themeConfig.configure({ theme })
+    this.state({ theme: updatedTheme })
+  }
+
+  render() {
+    return (
+      <Provider value={{ theme: this.state.themeConfig.theme }}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export { ThemeProvider }
+
+export default Consumer
