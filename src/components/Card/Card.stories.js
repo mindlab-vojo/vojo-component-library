0607b9573e7383bbe07d975470a852a10e9aff61
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 17:54:41 -0300

    Clean Card stories

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import Card from '.';
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
-import { withA11y } from '@storybook/addon-a11y';
+import { text } from "@storybook/addon-knobs";
 
-import { ThemeProvider } from './../../ThemeManager/ThemeProvider'
-import { redVojo } from './../../style/themes'
+import defaultDecorators from '../../../.storybook/decorators';
+import categories from '../../../.storybook/categories';
 
 export default {
-  title: 'Card',
+  title: categories.Card,
   component: Card,
-  decorators: [withKnobs, withA11y]
+  decorators: [...defaultDecorators]
 }
 
 export const component = () => {
   const description = text("description", "It's difficult to find examples of lorem ipsum in use before Letraset made it popular as a dummy text in the 1960s, although McClintock says he remembers coming across the lorem ipsum passage in a book of old metal type samples.")
   const image = text("image", "https://lastfm.freetls.fastly.net/i/u/770x0/5721b67079174fcccfe65965458b2559.jpg")
   const title = text("title", "Card Sample")
-  const redTheme = boolean("Red Themed", false)
   return (
-    <ThemeProvider theme={redTheme ? redVojo : null}>
-      <div>
-        <Card 
-          description={description} 
-          image={image}
-          title={title}
-        />
-      </div>
-    </ThemeProvider>
+    <Card 
+      description={description} 
+      image={image}
+      title={title}
+    />
   )
 }
 
