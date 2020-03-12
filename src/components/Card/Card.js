de2159aa4f50f357fab8ef5c2a8b75711d588e38
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 16:05:19 -0300

    Add tests to return components with consumer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,32 +6,35 @@ import * as Card from './Card.style'
 import ThemeConsumer from '../../style/themes/ThemeProvider'
 
 const CardComponent = ({ image, title, description }) => {
+
+  const renderComponent = (theme) => (
+    <Card.Wrapper 
+      className="Card__Wrapper"
+      theme={theme}>
+      <Card.UpperBody className="Card__Wrapper__UpperBody">
+        <Card.Image 
+          className="Card__Wrapper__UpperBody__Image"
+          src={image}
+        />
+      </Card.UpperBody>
+      <Card.LowerBody 
+        className="Card__Wrapper__LowerBody"
+        theme={theme}>
+        <Card.Title
+          className="Card__Wrapper__LowerBody__Title"
+          theme={theme}>
+          {title}
+        </Card.Title>
+        <Card.Description className="Card__Wrapper__LowerBody__Description">
+          {description}
+        </Card.Description>
+      </Card.LowerBody>
+    </Card.Wrapper>
+  )
+
   return (
     <ThemeConsumer>
-      {
-        ({ theme }) => (
-          <Card.Wrapper 
-            className="Card__Wrapper"
-            theme={theme}>
-            <Card.UpperBody className="Card__Wrapper__UpperBody">
-              <Card.Image 
-                className="Card__Wrapper__UpperBody__Image"
-                src={image}
-              />
-            </Card.UpperBody>
-            <Card.LowerBody className="Card__Wrapper__LowerBody">
-              <Card.Title
-                className="Card__Wrapper__LowerBody__Title"
-                theme={theme}>
-                {title}
-              </Card.Title>
-              <Card.Description className="Card__Wrapper__LowerBody__Description">
-                {description}
-              </Card.Description>
-            </Card.LowerBody>
-          </Card.Wrapper>
-        )
-      }
+      {({ theme }) => renderComponent(theme)}
     </ThemeConsumer>
   )
 }
