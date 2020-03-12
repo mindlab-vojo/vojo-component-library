71d07ede9dd0d8e850587d06e19735f61cd5332c
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 15:14:35 -0300

    Change colors

--- src/style/themes/redVojo/colors.js
+++ src/style/themes/redVojo/colors.js
@@ -1,6 +1,6 @@
-export const primaryColor = "#1B00BD"
-export const secondaryColor = "#FFFFFF"
+export const primaryColor = "#42240C"
+export const secondaryColor = "#FDBA9A"
 
-export const blackColor = "#212529"
+export const blackColor = "#42240C"
 export const whiteColor = "#FFFFFF"
 export const greyColor = "#E1E4E8"
