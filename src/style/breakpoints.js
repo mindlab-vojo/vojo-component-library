ecf78dbef91338f09aa617eef64cf6f12466a535
Leonardo Isso <leoisso.work@gmail.com>
2020-03-12 13:25:13 -0300

    Remove deprecated files

--- src/style/breakpoints.js
+++
@@ -1,6 +0,0 @@
-// Values stand for min-width > {const}
-export const tablet = "768px"
-export const desktop = "1024px"
-
-export const largerThanTablet = (style) => `@media only screen and (min-width: ${tablet}){${style}}`
-export const largerThanDesktop = (style) => `@media only screen and (min-width: ${desktop}){${style}}`
--- src/style/colors.js
+++
@@ -1,6 +0,0 @@
-export const primaryColor = "#1B00BD"
-export const secondaryColor = "#FFFFFF"
-
-export const blackColor = "#212529"
-export const whiteColor = "#FFFFFF"
-export const greyColor = "#E1E4E8"
