9ee869a31c33f438cc807f483260d065234886b0
Leonardo Isso <leoisso.work@gmail.com>
2020-03-17 14:48:48 -0300

    Add Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import * as Header from './Header.style'
 
+import Logo from './../Logo'
 import ThemeConsumer from '../../ThemeManager/ThemeProvider'
 
 function HeaderComponent() {
   const renderComponent = (theme) => (
     <Header.Wrapper theme={theme}>
       <Header.Content theme={theme}>
-        <p> LOL </p>
+        <Logo height="16px"/>
       </Header.Content>
     </Header.Wrapper>
   )
