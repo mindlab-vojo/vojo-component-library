5bae6df77d0955086e126bb34836ba295396deba
Leonardo Isso <leoisso.work@gmail.com>
2020-03-19 17:41:06 -0300

    Fix file name

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function TypographyComponent(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+TypographyComponent.propTypes = {
+
+}
+
+export default TypographyComponent
+
