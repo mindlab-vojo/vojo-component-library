ac398ea1cdfd17174b4996e634a08e6abbee1d9a
Leonardo Isso <leoisso.work@gmail.com>
2020-03-20 18:31:37 -0300

    Add fonts

---
+++ src/style/themes/vojo/fonts.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalFonts = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+  html {
+    font-family: 'Lato', sans-serif;
+  }
+`
+
+export default GlobalFonts
