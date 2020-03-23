94405ec5f89564b71c6e531653d9c9707e290b27
Leonardo Isso <leoisso.work@gmail.com>
2020-03-23 19:28:51 -0300

    Add redColor

--- src/style/themes/vojo/colors.js
+++ src/style/themes/vojo/colors.js
@@ -2,5 +2,6 @@ export const primaryColor = "#1B00BD"
 export const secondaryColor = "#FFFFFF"
 
 export const blackColor = "#212529"
-export const whiteColor = "#FFFFFF"
 export const greyColor = "#E1E4E8"
+export const redColor = "#E52312"
+export const whiteColor = "#FFFFFF"
