891e90c63ef80704d70daf4682e7276c159cf4c9
Leonardo Isso <leoisso.work@gmail.com>
2020-03-23 19:28:43 -0300

    Fix logic patterns of class

--- src/utils/validators/StringValidator.js
+++ src/utils/validators/StringValidator.js
@@ -1,3 +1,74 @@
+export default class StringValidator {
+
+  constructor(value, validatorPattern, options) {
+    this.errorMessage = ''
+    this.isValid = true
+    this.options = options
+    this.value = value
+    this.validatorPattern = validatorPatterns[validatorPattern]
+  }
+
+  checkLength = () => {
+    if (this.validatorPattern.minLength) {
+      this.checkMinLength()
+    }
+    if (this.validatorPattern.maxLength) {
+      this.checkMaxLength()
+    }
+  }
+
+  checkMinLength = () => {
+    const isValid = Boolean(this.value.length <= this.validatorPattern.minLength)
+    const errorMessage = isValid ? '' : `O valor deve ser maior que ${this.validatorPattern.minLength} caracteres`
+    this.setValidationProps(isValid, errorMessage)
+  }
+
+  checkMaxLength = () => {
+    const isValid = Boolean(this.value.length >= this.validatorPattern.maxLength)
+    const errorMessage = isValid ? '' : `O valor deve ser maior que ${this.validatorPattern.minLength} caracteres`
+    this.setValidationProps(isValid, errorMessage)
+  }
+
+  checkRegexPattern = () => {
+    const isValid = Boolean(this.value.match(this.validatorPattern.regex))
+    const errorMessage = isValid ? '' : this.validatorPattern.errorMessage
+    this.setValidationProps(isValid, errorMessage)
+  }
+  
+  setValidationProps = (isValid, errorMessage) => {
+    this.isValid = isValid
+    this.errorMessage = errorMessage
+  }
+
+  createValidationObject = () => {
+    return {
+      errorMessage: this.errorMessage,
+      isValid: this.isValid,
+      value: this.value,
+    }
+  }
+
+  validate = () => {
+    const validations = [
+      this.checkLength,
+      this.checkRegexPattern,
+    ]
+    
+    for (let i = 0; i < validations.length; i++) {
+      const validation = validations[i];
+      console.log(validation)
+      validation()
+
+      if (!this.isValid) {
+        return this.createValidationObject()
+      }
+    }
+
+    return this.createValidationObject()
+  }
+
+}
+
 export const validatorTypes = {
   alphaNumeric: 'alphaNumeric',
   alphaNumericSpaced: 'alphaNumericSpaced',
@@ -6,39 +77,28 @@ export const validatorTypes = {
 }
 
 export const validatorPatterns = {
-  [validatorTypes.alphaNumeric]: {
+  [validatorTypes.alphabeitcal]: {
+    errorMessage: 'O valor não deve possuir espaçamento, números ou símbolos',
     minLength: null,
     maxLength: null,
-    regex: /[A-Za-zÀ-ÖØ-öø-ÿ ]/,
-  }
+    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ]*$/,
+  },
+  [validatorTypes.alphabeitcalSpaced]: {
+    errorMessage: 'O valor não deve possuir números ou símbolos',
+    minLength: null,
+    maxLength: null,
+    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ ]*$/,
+  },
+  [validatorTypes.alphaNumeric]: {
+    errorMessage: 'O valor não deve possuir espaçamento ou símbolos',
+    minLength: null,
+    maxLength: null,
+    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ0-9]*$/,
+  },
+  [validatorTypes.alphaNumericSpaced]: {
+    errorMessage: 'O valor não deve possuir símbolos',
+    minLength: null,
+    maxLength: null,
+    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 ]*$/,
+  },
 }
-
-class StringValidator {
-
-  constructor(value, validatorPattern, options) {
-    this.value = value
-    this.validatorPattern = validatorPatterns[validatorPattern],
-    this.options = options
-  }
-
-  checkLength = () => {
-    if (this.validatorPattern.minLength) {
-      if (this.value.length <= this.validatorPattern.minLength){
-        return false
-      }
-    }
-    if (this.validatorPattern.maxLength) {
-      if (this.value.length >= this.validatorPattern.maxLength){
-        return false
-      }
-    }
-    return true
-  }
-
-  checkRegexPattern = () => {
-    
-  }
-
-}
-
-export default StringValidator
