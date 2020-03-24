c1cec850d4d7c6ebb08097dd20e421842cce2333
Leonardo Isso <leoisso.work@gmail.com>
2020-03-24 11:20:53 -0300

    Fix Typography dynamic class generation

--- src/components/Typography/Typography.style.js
+++ src/components/Typography/Typography.style.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React,  { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-export const Typography = ({ additionalProps, children, tag, typoColor, typoStyles, isUnderlined }) => {
-  const Component = styled(tag).attrs(additionalProps)`
-    color: ${typoColor};
-    font-size: ${typoStyles.fontSize};
-    margin: 0;
-    ${isUnderlined && `text-decoration: underline;`}
-  `
+export class Typography extends Component {
+  static propTypes = {
+    additionalProps: PropTypes.object,
+    children: PropTypes.node,
+    color: PropTypes.string, 
+    isUnderlined: PropTypes.bool,
+    tag: PropTypes.string,
+    typoColor: PropTypes.string,
+    typoStyles: PropTypes.object,
+  }
 
-  return (
-    <Component>
-      {children}
-    </Component>
-  )
-}
+  render() {
+    const { additionalProps, children, tag, typoColor, typoStyles, isUnderlined } = this.props;
 
-Typography.propTypes = {
-  additionalProps: PropTypes.object,
-  children: PropTypes.node,
-  color: PropTypes.string, 
-  isUnderlined: PropTypes.bool,
-  tag: PropTypes.string,
-  typoColor: PropTypes.string,
-  typoStyles: PropTypes.object,
+    const Component = styled(tag).attrs(additionalProps)`
+      color: ${typoColor};
+      font-size: ${typoStyles.fontSize};
+      margin: 0;
+      ${isUnderlined && `text-decoration: underline;`}
+    `
+
+    return (
+      <Component>
+        {children}
+      </Component>
+    )
+  }
 }
