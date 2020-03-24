15739ea6bc74abfd02a18849e0ab02f1514410dd
Leonardo Isso <leoisso.work@gmail.com>
2020-03-24 09:29:33 -0300

    Split Validator and type files

--- src/utils/validators/StringValidator.js
+++ src/utils/validators/StringValidator.js
@@ -1,4 +1,6 @@
-export default class StringValidator {
+import { validatorPatterns } from './validatorTypes'
+
+class StringValidator {
 
   constructor(value, validatorPattern, options) {
     this.errorMessage = ''
@@ -75,43 +77,6 @@ export default class StringValidator {
 
     return this.createValidationObject()
   }
-
 }
 
-export const validatorTypes = {
-  alphaNumeric: 'alphaNumeric',
-  alphaNumericSpaced: 'alphaNumericSpaced',
-  alphabeitcal: 'alphabeitcal',
-  alphabeitcalSpaced: 'alphabeitcalSpaced',
-}
-
-export const validatorPatterns = {
-  [validatorTypes.alphabeitcal]: {
-    errorMessage: 'O valor não deve possuir espaçamento, números ou símbolos',
-    minLength: null,
-    maxLength: null,
-    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ]*$/,
-    specificValidation: null,
-  },
-  [validatorTypes.alphabeitcalSpaced]: {
-    errorMessage: 'O valor não deve possuir números ou símbolos',
-    minLength: null,
-    maxLength: null,
-    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ ]*$/,
-    specificValidation: null,
-  },
-  [validatorTypes.alphaNumeric]: {
-    errorMessage: 'O valor não deve possuir espaçamento ou símbolos',
-    minLength: null,
-    maxLength: null,
-    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ0-9]*$/,
-    specificValidation: null,
-  },
-  [validatorTypes.alphaNumericSpaced]: {
-    errorMessage: 'O valor não deve possuir símbolos',
-    minLength: null,
-    maxLength: null,
-    regex: /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 ]*$/,
-    specificValidation: null,
-  },
-}
+export default StringValidator
