d3edad6892ed37fc6279748c47bd14c9cbeefce2
Leonardo Isso <leoisso.work@gmail.com>
2020-03-24 17:30:42 -0300

    Export components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import Button from './components/Button';
 import Logo from './components/Logo';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import TextInput from './components/Input/TextInput';
 import Typography from './components/Typography';
 
 import ThemeConsumer, { ThemeProvider } from './style/ThemeManager/ThemeProvider'
@@ -8,9 +10,11 @@ import ThemeConsumer, { ThemeProvider } from './style/ThemeManager/ThemeProvider
 import * as themes from './style/themes'
 
 export {
+  Button,
   Logo,
   Footer,
   Header,
+  TextInput,
   Typography,
   ThemeConsumer,
   ThemeProvider,
