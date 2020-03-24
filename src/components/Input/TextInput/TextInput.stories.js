fa4078191fe6ccd76c7d8fb3ac3fb59732c32945
Leonardo Isso <leoisso.work@gmail.com>
2020-03-24 10:44:09 -0300

    Add props

--- src/components/Input/TextInput/TextInput.stories.js
+++ src/components/Input/TextInput/TextInput.stories.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
+import { action } from '@storybook/addon-actions'
 
 import TextInput from '.'; 
 import { ThemeProvider } from "../../../../src";
 import categories from '../../../../.storybook/categories';
 
 export const component = () => {
-  return <TextInput id="lol"/>
+  const disabled = boolean('disabled', false)
+  const id = text('id', 'vojo-input')
+  const name =  text('name', 'vojo-input')
+  const onInputBlur = action('onInputBlur')
+  const onInputChange = action('onInputChange')
+  const placeholder =  text('placeholder', '')
+  const type =  text('type', 'text')
+  const value =  text('value', '')
+
+  return (
+    <TextInput 
+      disabled={disabled}
+      id={id}
+      name={name}
+      onInputBlur={onInputBlur}
+      onInputChange={onInputChange}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+    />
+  )
 }
 
 export default {
