50993a7ff5998432a12af48df584896f408db527
Leonardo Isso <leoisso.work@gmail.com>
2020-04-15 15:05:11 -0300

    Add initial icons

---
+++ src/components/IconManager/Icons/VisibilityOn.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const VisibilityOn = ({
+  width="24", 
+  height="24",
+  fill
+}) => {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      height={height} 
+      viewBox="0 0 24 24" 
+      width={width}>
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path 
+        fill={fill}
+        d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+    </svg>
+  )
+}
+
+VisibilityOn.propTypes = {
+  width: PropTypes.string, 
+  height: PropTypes.string, 
+  fill: PropTypes.string
+}
+
+export default VisibilityOn
