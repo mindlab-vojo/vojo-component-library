a414fc394703d3beaceb343c344f103c63f310f5
Leonardo Isso <leoisso.work@gmail.com>
2020-04-22 14:43:13 -0300

    Improve CheckboxComponent

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -3,34 +3,55 @@ import PropTypes from 'prop-types'
 
 import * as Checkbox from './Checkbox.style'
 
+import IconManager from './../IconManager'
+
 const CheckboxComponent = ({
+  id,
   isChecked,
-  inClickCheckbox,
-  value, 
+  onClickCheckbox,
+  label,
+  name,
+  value,
 }) => {
-  
   const [checked, setChecked] = useState(false)
+  const componentName = name ? name : id
 
   if (isChecked) {
     setChecked(true)
   }
+  
+  const handleCheckboxClick = () => {
+    setChecked(!checked)
+    onClickCheckbox && onClickCheckbox(checked)
+  }
 
   const renderCheckbox = () => checked ? (
-    null
+    <IconManager icon="CheckboxChecked"/>
   ) : (
-    null
+    <IconManager icon="CheckboxUnchecked"/>
   )
 
   return (
-    <Checkbox.Wrapper>
-        
+    <Checkbox.Wrapper
+      onClick={() => handleCheckboxClick()}>
+      <Checkbox.Checkbox 
+        id={id}
+        name={componentName}
+        type="checkbox"/>
+      {renderCheckbox()}
+      <Checkbox.Label htmlFor={id}>
+        {label}
+      </Checkbox.Label>
     </Checkbox.Wrapper>
   )
   
 }
 
 CheckboxComponent.propTypes = {
-  prop: PropTypes
+  isChecked: PropTypes.bool,
+  onClickCheckbox: PropTypes.func,
+  label: PropTypes.node,
+  value: PropTypes.node,
 }
 
 export default CheckboxComponent
