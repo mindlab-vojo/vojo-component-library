7fd19e778eb3cd7eaa70b2fa7e6e9e2493945565
Leonardo Isso <leoisso.work@gmail.com>
2020-04-22 15:06:30 -0300

    Add component style

--- src/components/Checkbox/Checkbox.style.js
+++ src/components/Checkbox/Checkbox.style.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  cursor: pointer;
+  align-items: center;
+  display: flex;
+  width: 100%;
+`
 
-export const Label = styled.label``
+export const Label = styled.label`
+  cursor: inherit;
+  margin-left: 5px;
+`
 
 export const Checkbox = styled.input`
   display: none;
