0a480a832575b95e955a2604e42d8fd807d7dd39
Leonardo Isso <leoisso.work@gmail.com>
2020-04-23 15:51:16 -0300

    Add base files

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CardComponent(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+CardComponent.propTypes = {
+
+}
+
+export default CardComponent
+
