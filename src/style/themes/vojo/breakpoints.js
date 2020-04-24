00c1d74242dd63d020269e76632a0ed8f5dbed31
Leonardo Isso <leoisso.work@gmail.com>
2020-04-24 16:22:56 -0300

    Change mobile size

--- src/style/themes/vojo/breakpoints.js
+++ src/style/themes/vojo/breakpoints.js
@@ -3,6 +3,6 @@ export const tablet = "768px"
 export const desktop = "1024px"
 
 // Grid sizes
-export const mobileContentSize = "425px"
+export const mobileContentSize = "320px"
 export const desktopContentSize = "960px";
 export const fullContentSize = "100%";
