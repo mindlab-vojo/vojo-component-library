06522e5ed72a08b237dcf05f19a72530d621b13e
Leonardo Isso <leoisso.work@gmail.com>
2020-04-27 11:57:11 -0300

    Improve component

--- src/components/StickyToast/StickyToast.js
+++ src/components/StickyToast/StickyToast.js
@@ -1,38 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Sticky from 'react-sticky-el'
 
-import * as StickyToast from './Footer.style'
+import * as StickyToast from './StickyToast.style'
 
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
 import IconManager from '../IconManager'
 
 const StickyToastComponent = ({
-  children
+  children,
+  show,
 }) => {
   const renderComponent = theme => (
-    <StickyToast.Wrapper 
-      className="StickyToast"
-      theme={theme}>
-      <StickyToast.Content 
-        className="StickyToast__Content">
-        {children}
-      </StickyToast.Content>
-      <StickyToast.Close 
-        className="StickyToast__Close">
-        <IconManager width="24" icon="ArrowLeft"/>
-      </StickyToast.Close>
-    </StickyToast.Wrapper>
+    <Sticky>
+      <StickyToast.Wrapper 
+        className="StickyToast"
+        theme={theme}>
+        <StickyToast.Content 
+          className="StickyToast__Content">
+          {children}
+        </StickyToast.Content>
+        <StickyToast.Close 
+          className="StickyToast__Close">
+          <IconManager width="24" icon="ArrowLeft"/>
+        </StickyToast.Close>
+      </StickyToast.Wrapper>
+    </Sticky>
   )  
 
-  return (
+  return show ? (
     <ThemeConsumer>
       {({ theme }) => renderComponent(theme)}
     </ThemeConsumer>
-  )
+  ) : null
 }
 
 StickyToastComponent.propTypes = {
   children: PropTypes.node,
+  show: PropTypes.bool,
 }
 
 export default StickyToastComponent
