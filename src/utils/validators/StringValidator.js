b517952e022b5f65d148abf34bf98209bb455714
Leonardo Isso <leoisso.work@gmail.com>
2020-05-26 18:58:39 -0300

    Remove console log

--- src/utils/validators/StringValidator.js
+++ src/utils/validators/StringValidator.js
@@ -13,7 +13,6 @@ class StringValidator {
 
   checkIsRequired = () => {
     if (this.options.isRequired) {
-      console.log((this.value && this.value.length > 0))
       const isValid = Boolean((this.value !== '') && (this.value.length > 0))
       const errorMessage = isValid ? '' : `Este campo é obrigatório`
       this.setValidationProps(isValid, errorMessage)
