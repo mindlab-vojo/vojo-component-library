8216d3bb574f85cdd4e46ed6bedbf5cfe5b66915
Leonardo Isso <leoisso.work@gmail.com>
2020-06-03 16:57:42 -0300

    Add basic components

--- src/components/FilterDropdown/FilterDropdown.style.js
+++ src/components/FilterDropdown/FilterDropdown.style.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+${props => props.isSelected 
+    ? `
+    background-color: ${props.theme.colors.whiteColor};
+    ` : `
+    background-color: ${props.theme.colors.blackColor};
+    `
+}
+  align-items: center;
+  border: 2px solid ${props => props.theme.colors.blackColor};
+  border-radius: 5px;
+  display: flex;
+  padding: 12px 15px;
+  width: 100%
+`
+export const Text = styled.div``
+
+export const Icon = styled.div``
