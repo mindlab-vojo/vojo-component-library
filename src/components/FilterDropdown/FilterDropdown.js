cdd315a9075f9dba8de3285d74e4970263a5e07b
Leonardo Isso <leoisso.work@gmail.com>
2020-06-03 18:40:22 -0300

    Add component logic

--- src/components/FilterDropdown/FilterDropdown.js
+++ src/components/FilterDropdown/FilterDropdown.js
@@ -15,37 +15,78 @@ const FilterDropdownComponent = ({
 }) => {
   const [isSelected, setIsSelected] = useState(Boolean(defaultOption))
   const [isOpen, setIsOpen] = useState(false)
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  const handleDropdownClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleOptionSelect = (option) => {
+    setIsSelected(true)
+    setSelectedOption(option)
+    setIsOpen(false)
+    onSelectOption && onSelectOption(option)
+  }
 
   const renderComponent = theme => {
     const currentColor = isSelected 
-      ? theme.colors.blackColor
-      : theme.colors.whiteColor
+      ? theme.colors.whiteColor
+      : theme.colors.blackColor
 
     const currentIcon = isOpen 
       ? 'ArrowUp'
       : 'ArrowDown'
     
     return (
-      <FilterDropdown.Wrapper
-        className="FilterDropdown"
-        isSelected={isSelected}
-        theme={theme}>
-        <FilterDropdown.Text
-          className="FilterDropdown__Text">
-          <Typography 
-            color={currentColor}
-            tag="p"
-            type="default">
-            TESTE
-          </Typography>
-        </FilterDropdown.Text>
-        <FilterDropdown.Icon
-          className="FilterDropdown__Icon">
-          <IconManager 
-            icon={currentIcon}
-            fill={currentColor}
-          />
-        </FilterDropdown.Icon>
+      <FilterDropdown.Wrapper>
+        <FilterDropdown.Input
+          className="FilterDropdown"
+          isSelected={isSelected}
+          onClick={() => handleDropdownClick()}
+          theme={theme}>
+          <FilterDropdown.Text
+            className="FilterDropdown__Text">
+            <Typography 
+              color={currentColor}
+              tag="p"
+              type="default">
+              {isSelected ? selectedOption.name : placeholder}
+            </Typography>
+          </FilterDropdown.Text>
+          <FilterDropdown.Icon
+            className="FilterDropdown__Icon">
+            <IconManager 
+              icon={currentIcon}
+              fill={currentColor}
+            />
+          </FilterDropdown.Icon>
+        </FilterDropdown.Input>
+        {
+          isOpen
+            ? (
+              <FilterDropdown.Dropdown 
+                className="FilterDropdown__Dropdown"
+                theme={theme}>
+                {
+                  options.map(option => (
+                    <FilterDropdown.Item
+                      key={option.id}
+                      className="FilterDropdown__Item"
+                      onClick={() => handleOptionSelect(option)}
+                      theme={theme}>
+                      <Typography 
+                        color={theme.colors.whiteColor}
+                        tag="p"
+                        type="default">
+                        {option.name}
+                      </Typography>
+                    </FilterDropdown.Item>
+                  ))
+                }
+              </FilterDropdown.Dropdown>
+            ) : null
+        }
+        
       </FilterDropdown.Wrapper>
     )}
 
