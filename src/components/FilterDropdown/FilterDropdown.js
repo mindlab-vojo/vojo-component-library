8d30cbf97dce77465c7738bcbdd89f9e9699c1d4
Leonardo Isso <leoisso.work@gmail.com>
2020-06-03 16:58:58 -0300

    Add initial body

--- src/components/FilterDropdown/FilterDropdown.js
+++ src/components/FilterDropdown/FilterDropdown.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
+import Typography from '../Typography'
+import IconManager from '../IconManager'
 import * as FilterDropdown from './FilterDropdown.style'
 
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
 
 const FilterDropdownComponent = ({
+  defaultOption,
   options,
   onSelectOption,
+  placeholder,
 }) => {
+  const [isSelected, setIsSelected] = useState(Boolean(defaultOption))
 
-  const renderComponent = theme => (
-    <FilterDropdown.Wrapper>
-
-    </FilterDropdown.Wrapper>
-  )
+  const renderComponent = theme => {
+    
+    const currentColor = isSelected 
+      ? theme.colors.blackColor
+      : theme.colors.whiteColor
+    return (
+      <FilterDropdown.Wrapper
+        isSelected={isSelected}
+        theme={theme}>
+        <FilterDropdown.Text>
+          <Typography 
+            color={currentColor}
+            tag="p"
+            type="default">
+            TESTE
+          </Typography>
+        </FilterDropdown.Text>
+        <FilterDropdown.Icon>
+          <IconManager 
+            icon="ArrowDown"
+            fill={currentColor}
+          />
+        </FilterDropdown.Icon>
+      </FilterDropdown.Wrapper>
+    )}
 
   return (
     <ThemeConsumer>
@@ -24,8 +49,10 @@ const FilterDropdownComponent = ({
 }
 
 FilterDropdownComponent.propTypes = {
+  defaultOption: PropTypes.any,
   options: PropTypes.object,
-  onSelectOption: PropTypes.func
+  onSelectOption: PropTypes.func,
+  placeholder: PropTypes.string,
 }
 
 export default FilterDropdownComponent
