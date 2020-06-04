a7661e9174324398c6fe8c1438643cb07773562a
Leonardo Isso <leoisso.work@gmail.com>
2020-06-04 19:10:06 -0300

    Add structure to component

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import * as JobCard from './JobCard.style'
 
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
+import { Card } from '../Card/Card.style'
+import Typography from '../Typography'
 
 const JobCardComponent = ({
   companyNumber,
@@ -16,10 +18,39 @@ const JobCardComponent = ({
   slots,
   title,
 }) => {
+
+  const slotText = slots > 1 ? "VAGAS" : "VAGA"
+
   const renderComponent = (theme) => (
-    <JobCard.Wrapper>
-      
-    </JobCard.Wrapper>
+    <Card padding="12px 8px">
+      <JobCard.Wrapper
+        className="JobCard">
+        <JobCard.ImageBlock 
+          className="JobCard__ImageBlock">
+          <JobCard.ImageWrapper
+            className="JobCard__ImageWrapper">
+            <JobCard.ImageCropper
+              className="JobCard__ImageWrapper__Cropper">
+              <JobCard.Image
+                src={imageUrl}
+                alt={title}
+              />
+            </JobCard.ImageCropper>
+          </JobCard.ImageWrapper>
+          <JobCard.Slots
+            className="JobCard__Slots">
+            <Typography 
+              fontSize="10px"
+              fontWeight="bolder">
+              {`${slots} ${slotText}`}
+            </Typography>
+          </JobCard.Slots>
+        </JobCard.ImageBlock>
+        <JobCard.DataBlock
+          className="JobCard__DataBlock">
+        </JobCard.DataBlock>
+      </JobCard.Wrapper>
+    </Card>
   )
 
   return (
--- src/components/JobCard/JobCard.style.js
+++ src/components/JobCard/JobCard.style.js
@@ -1,3 +1,43 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+`
+
+export const ImageBlock = styled.div`
+  width: 72px;
+`
+
+export const ImageWrapper = styled.div``
+
+export const ImageCropper = styled.div`
+  width: 72px;
+  height: 72px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+`
+
+export const Image = styled.img`
+  display: inline;
+  margin: 0 auto;
+  height: 100%;
+  width: auto;
+  z-index: 1;
+`
+
+export const Slots = styled.div`
+  background-color: #FFE010;
+  border-radius: 5px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  margin-top: -17px;
+  padding: 5px;
+  position: relative;
+  width: 72px;
+  z-index: 2;
+`
+
+export const DataBlock = styled.div``
