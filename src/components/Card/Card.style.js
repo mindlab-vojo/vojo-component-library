abaf85f9d7219b265fb112a6fe10f7665eff5fd8
Leonardo Isso <leoisso.work@gmail.com>
2020-06-04 19:09:47 -0300

    Add Card padding prop

--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Card = styled.div`
   border-radius: 6px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
-  padding: 14px 10px;
+  padding: ${props => props.padding};
   overflow: hidden;
   width: 100%;
 `
