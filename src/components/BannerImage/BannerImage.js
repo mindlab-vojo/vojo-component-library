943b73795e9a600f84820d66a41e19bb16477731
Leonardo Isso <leoisso.work@gmail.com>
2020-06-18 17:29:10 -0300

    Add BannerImage base component

---
+++ src/components/BannerImage/BannerImage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Typography from '../Typography'
+
+import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
+import * as BannerImage from './BannerImage.style'
+
+const BannerImageComponent = ({
+  imageUrl,
+  title,
+}) => {
+
+  const renderComponent = (theme) => (
+    <BannerImage.Wrapper
+      className="BannerImage">
+      <BannerImage.Cropper
+        className="BannerImage__Cropper">
+        <BannerImage.Image
+          className="BannerImage__Image"
+          src={imageUrl}/>
+      </BannerImage.Cropper>
+      <BannerImage.Overlay/>
+      <BannerImage.Title>
+        <Typography>
+          {title}
+        </Typography>
+      </BannerImage.Title>
+    </BannerImage.Wrapper>
+  )
+  return (
+    <ThemeConsumer>
+      {({ theme }) => renderComponent(theme)}
+    </ThemeConsumer>
+  )
+}
+
+BannerImageComponent.propTypes = {
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+}
+
+export default BannerImageComponent
---
+++ src/components/BannerImage/BannerImage.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div``
+
+export const Overlay = styled.div``
+
+export const Cropper = styled.div``
+
+export const Image = styled.img``
+
+export const Title = styled.div``
+
