7d098767bdf9e27e389df9e858109e465d4b0701
Leonardo Isso <leoisso.work@gmail.com>
2020-07-02 15:54:33 -0300

    Add Toast base component

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import * as Toast from './Toast.style'
+
+import Typography from '../Typography'
+
+import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
+
+const ToastComponent = ({
+  description,
+  title,
+}) => {
+  const renderComponent = (theme) => (
+    <Toast.Wrapper>
+      <Toast.Content>
+        <Toast.Title>
+          <Typography
+            color={theme.colors.redColor}
+            fontSize="12px"
+            fontWeight="bolder">
+            {title}
+          </Typography>
+        </Toast.Title>
+        <Toast.Description>
+          <Typography
+            color={theme.colors.redColor}
+            fontSize="12px">
+            {description}
+          </Typography>
+        </Toast.Description>
+      </Toast.Content>
+    </Toast.Wrapper>
+  )
+  
+  return (
+    <ThemeConsumer>
+      {({ theme }) => renderComponent(theme)}
+    </ThemeConsumer>
+  )
+}
+
+ToastComponent.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+}
+
+export default ToastComponent
