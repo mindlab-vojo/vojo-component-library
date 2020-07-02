483c0492096fce97fca4ce043802ecedcdfe2fc5
Leonardo Isso <leoisso.work@gmail.com>
2020-07-02 15:57:55 -0300

    Style Toast

--- src/components/Toast/Toast.style.js
+++ src/components/Toast/Toast.style.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  background: ${props => props.theme.colors.superLightRedColor};
+  border: 1px solid ${props => props.theme.colors.redColor};
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 15px
+`
 
 export const Content = styled.div``
 
