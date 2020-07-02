9872cc884510fd02a5d810a537b5077808127642
Leonardo Isso <leoisso.work@gmail.com>
2020-07-02 13:24:04 -0300

    Structure and style component

--- src/components/ModuleCard/ModuleCard.js
+++ src/components/ModuleCard/ModuleCard.js
@@ -2,22 +2,68 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Card from '../Card'
+import IconManager from '../IconManager'
 import Typography from '../Typography'
 
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
-import * as ModuleCard from './BannerImage.style'
+import * as ModuleCard from './ModuleCard.style'
 
 const ModuleCardComponent = ({
   description,
-  imageUrl,
+  iconName,
+  iconBackgroundColor,
   onClickCard,
   title,
   time,
 }) => {
 
   const renderComponent = (theme) => (
-    <Card>
-
+    <Card
+      hasPointer
+      onClickCard={onClickCard}>
+      <ModuleCard.Wrapper
+        className="ModuleCard">
+        <ModuleCard.IconWrapper
+          className="ModuleCard__IconWrapper">
+          <ModuleCard.IconCircle
+            backgroundColor={iconBackgroundColor}
+            className="ModuleCard__IconCircle">
+            <IconManager
+              height="24px"
+              fill={theme.colors.whiteColor}
+              icon={iconName}
+              width="24px"
+            />
+          </ModuleCard.IconCircle>
+        </ModuleCard.IconWrapper>
+        <ModuleCard.InfoWrapper
+          className="ModuleCard__InfoWrapper">
+          <ModuleCard.Title
+            className="ModuleCard__InfoWrapper__Title">
+            <Typography
+              color={theme.colors.darkBlueColor}
+              fontSize="18px"
+              fontWeight="bolder">
+              {title}
+            </Typography>
+          </ModuleCard.Title>
+          <ModuleCard.Description
+            className="ModuleCard__InfoWrapper__Description">
+            <Typography
+              fontSize="12px">
+              {description}
+            </Typography>
+          </ModuleCard.Description>
+          <ModuleCard.Time
+            className="ModuleCard__InfoWrapper__Time">
+            <Typography
+              fontSize="12px"
+              fontWeight="bolder">
+              {time}
+            </Typography>
+          </ModuleCard.Time>
+        </ModuleCard.InfoWrapper>
+      </ModuleCard.Wrapper>
     </Card>
   )
   return (
@@ -29,6 +75,8 @@ const ModuleCardComponent = ({
 
 ModuleCardComponent.propTypes = {
   description: PropTypes.string,
+  iconName: PropTypes.string,
+  iconBackgroundColor: PropTypes.string,
   imageUrl: PropTypes.string,
   onClickCard: PropTypes.func,
   title: PropTypes.string,
