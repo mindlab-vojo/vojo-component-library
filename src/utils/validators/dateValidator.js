5445776019936e738f6b8b7a83c1942c7e6bb711
Leonardo Isso <leoisso.work@gmail.com>
2020-08-18 16:40:00 -0300

    Add dateValidatior

---
+++ src/utils/validators/dateValidator.js
@@ -0,0 +1,11 @@
+import moment from "moment"
+
+export const dateMaskRemover = (value) => {
+  const date = value.split('/').reverse().join('/')
+  return date
+}
+
+export const dateValidator = (value) => {
+  const date = dateMaskRemover(value)
+  return moment(date).isValid()
+}
