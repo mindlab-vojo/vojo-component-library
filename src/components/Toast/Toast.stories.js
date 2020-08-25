2b43505eb598a099ebf846b008bdd85260d4b52f
Leonardo Isso <leoisso.work@gmail.com>
2020-08-25 12:12:32 -0300

    Add Toast types to storybook

--- src/components/Toast/Toast.stories.js
+++ src/components/Toast/Toast.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 
 import Toast from '.'; 
@@ -9,7 +9,14 @@ import categories from '../../../.storybook/categories';
 import Container from '../Container';
 
 export const toast = () => {  
+
+  const types = [
+    'error',
+    'warn'
+  ]
+
   const title = text('title', 'Formulário de inscrição')
+  const type = select('type', types, types[0])
   const description = text('description', (
     `- Como esta oportunidade necessita de um veículo, é preciso que você tenha uma CNH válida para exercer a função.
     - Algumas empresas priorizam usuários com MEI. Portanto se você quiser abrir um MEI e aumentar as suas chances acesse: www.vojo.app/mei. Depois volte para o site do VOJO e atualize o seu cadastro. 
@@ -32,7 +39,8 @@ export const toast = () => {
     <Container maxWidth="desktop">
       <Toast 
         description={descriptionToComponent()}
-        title={title}/>
+        title={title}
+        type={type}/>
     </Container>
   )
 }
