6ec2cbff9c78bfcbf2ef49e70d8ff09847fb4e7c
Leonardo Isso <leoisso.work@gmail.com>
2020-08-26 12:01:37 -0300

    ADd applicationCard to storybook

---
+++ src/components/ApplicationCard/ApplicationCard.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { action } from '@storybook/addon-actions'
+
+import ApplicationCard from '.'; 
+import { ThemeProvider } from "../../../src";
+import categories from '../../../.storybook/categories';
+
+export const applicationCard = () => {
+  const currentStep = text("currentStep", "1")
+  const jobTitle = text("jobTitle", "Seletor de produtos para entrega (Shopper)")
+  const imageUrl = text("imageUrl", "https://russelservicos.com.br/wp-content/uploads/2016/01/balconista1.jpg")  
+  const isClosed = boolean("isClosed", false)  
+  const onClickCard = action("onClickCard")
+  const stepTitle = text("stepTitle", "Procurando empresas")
+  const totalSteps = text("totalSteps", "4")
+
+  return (
+    <ApplicationCard
+      currentStep={currentStep}
+      imageUrl={imageUrl}
+      isClosed={isClosed}
+      jobTitle={jobTitle}
+      onClickCard={onClickCard}
+      stepTitle={stepTitle}
+      totalSteps={totalSteps}
+    />
+  )
+}
+
+export default {
+  title: categories.Cards,
+  name: 'ApplicationCard',
+  component: applicationCard,
+  decorators: [
+    withKnobs, 
+    withA11y, 
+    (story) => <ThemeProvider>{ story() }</ThemeProvider>,
+    (story) => <div style={{ display: "flex" }}>{ story() }</div>,
+  ],
+}
+  
