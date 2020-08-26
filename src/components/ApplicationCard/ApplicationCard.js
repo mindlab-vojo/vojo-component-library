47769403410f364c2f96b4aeaebe4e4f3d402189
Leonardo Isso <leoisso.work@gmail.com>
2020-08-26 12:14:30 -0300

    Add component rendering base

---
+++ src/components/ApplicationCard/ApplicationCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Card from '../Card'
+import Typography from '../Typography'
+
+import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
+import * as ApplicationCard from './ApplicationCard.style'
+
+const ApplicationCardComponent = ({
+  currentStep,
+  jobTitle,
+  imageUrl,
+  isClosed,
+  onClickCard,
+  stepTitle,
+  totalSteps,
+}) => {
+
+  const renderComponent = (theme) => (
+    <Card
+      hasPointer
+      onClickCard={onClickCard}>
+      <ApplicationCard.Wrapper
+        className="ApplicationCard">
+          
+
+      </ApplicationCard.Wrapper>
+    </Card>
+  )
+  return (
+    <ThemeConsumer>
+      {({ theme }) => renderComponent(theme)}
+    </ThemeConsumer>
+  )
+}
+
+ApplicationCardComponent.propTypes = {
+  currentStep: PropTypes.string,
+  jobTitle: PropTypes.string,
+  imageUrl: PropTypes.string,
+  isClosed: PropTypes.bool,
+  onClickCard: PropTypes.func,
+  stepTitle: PropTypes.string,
+  totalSteps: PropTypes.string,
+}
+
+export default ApplicationCardComponent
---
+++ src/components/ApplicationCard/ApplicationCard.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  width: 100%;
+`
