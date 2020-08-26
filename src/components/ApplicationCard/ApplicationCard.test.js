ae712241cba34f9cdc93d31a99d1e576e8068ab4
Leonardo Isso <leoisso.work@gmail.com>
2020-08-26 16:15:28 -0300

    Add tests

---
+++ src/components/ApplicationCard/ApplicationCard.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ApplicationCard from './';
+import { ThemeProvider } from './../../style/ThemeManager/ThemeProvider';
+
+test('Test ApplicationCard component render', () => {
+  const fakeFn = jest.fn()
+  const component = render(
+    <ThemeProvider>
+      <ApplicationCard 
+        currentStep={"1"}
+        jobTitle={"Seletor de produtos para entrega (Shopper)"}
+        imageUrl={"https://russelservicos.com.br/wp-content/uploads/2016/01/balconista1.jpg"}
+        isClosed={false}
+        onClickCard={fakeFn}
+        stepTitle={"Procurando empresas"}
+        totalSteps={"4"}
+      />
+    </ThemeProvider>
+  );
+  expect(component).toMatchSnapshot()
+});
