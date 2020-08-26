cde3693ce019f43961270b613b23d076c1b9798e
Leonardo Isso <leoisso.work@gmail.com>
2020-08-26 17:47:17 -0300

    Add initial body

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,21 +1,70 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
+import IconManager from '../IconManager'
 import Typography from '../Typography'
 
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
 import * as SideMenu from './SideMenu.style'
 
 const SideMenuComponent = ({
+  buttons,
   items,
   onClickClose,
   onClickOutside
 }) => {
 
+  const renderItems = (theme) => {
+    return items.map((item, index) => (
+      <SideMenu.Item
+        onClick={() => item.func && item.func()}
+        key={index}>
+        <SideMenu.ItemIcon>
+          <IconManager
+            fill={theme.colors.primaryColor}
+            icon={item.icon}
+            width="22px"
+          />
+        </SideMenu.ItemIcon>
+        <SideMenu.ItemText>
+          <Typography
+            color={theme.colors.primaryColor}
+            fontSize="18px"
+            fontWeight="bolder">
+            {item.text}
+          </Typography>
+        </SideMenu.ItemText>
+      </SideMenu.Item>
+    ))
+  }
+
   const renderComponent = (theme) => (
-    <>
-    
-    </>
+    <SideMenu.Wrapper
+      className="SideMenu">
+      <SideMenu.Background
+        className="SideMenu__Background"/>
+      <SideMenu.Menu
+        className="SideMenu__Menu">
+        <SideMenu.Close
+          className="SideMenu__CloseWrapper">
+          <SideMenu.CloseIcon
+            className="SideMenu__Close"
+            onClick={onClickClose}>
+            <IconManager
+              fill={theme.colors.blackColor}
+              icon="Close"
+              width="25px"
+            />
+          </SideMenu.CloseIcon>
+        </SideMenu.Close>
+        <SideMenu.MenuItems>
+          {renderItems(theme)}
+        </SideMenu.MenuItems>
+        <SideMenu.MenuButtons>
+            
+        </SideMenu.MenuButtons>
+      </SideMenu.Menu>
+    </SideMenu.Wrapper>
   )
 
   return (
@@ -26,6 +75,10 @@ const SideMenuComponent = ({
 }
 
 SideMenuComponent.propTypes = {
+  buttons: PropTypes.array,
+  items: PropTypes.array,
+  onClickClose: PropTypes.func,
+  onClickOutside: PropTypes.func
 }
 
 export default SideMenuComponent
