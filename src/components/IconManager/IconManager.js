f7c6a72577439edb53b98a92601bac4f4ce63bb4
Leonardo Isso <leoisso.work@gmail.com>
2020-08-27 09:15:24 -0300

    Export new icons

--- src/components/IconManager/IconManager.js
+++ src/components/IconManager/IconManager.js
@@ -5,16 +5,21 @@ import ArrowDown from './Icons/ArrowDown'
 import ArrowLeft from './Icons/ArrowLeft'
 import ArrowRigth from './Icons/ArrowRigth'
 import ArrowUp from './Icons/ArrowUp'
+import Avatar from './Icons/Avatar'
 import Check from './Icons/Check'
 import CheckboxChecked from './Icons/CheckboxChecked'
 import CheckboxUnchecked from './Icons/CheckboxUnchecked'
 import Close from './Icons/Close'
 import Exclamation from './Icons/Exclamation'
+import Hamburger from './Icons/Hamburger'
 import HelpCircle from './Icons/HelpCircle'
 import Lock from './Icons/Lock'
 import Pencil from './Icons/Pencil'
+import Question from './Icons/Question'
 import RadioChecked from './Icons/RadioChecked'
 import RadioUnchecked from './Icons/RadioUnchecked'
+import Suitcase from './Icons/Suitcase'
+import SuitcaseArrow from './Icons/SuitcaseArrow'
 import VisibilityOff from './Icons/VisibilityOff'
 import VisibilityOn from './Icons/VisibilityOn'
 
@@ -39,6 +44,7 @@ IconManager.propTypes = {
 }
 
 export const iconList = {
+  Avatar: Avatar,
   ArrowDown: ArrowDown,
   ArrowLeft: ArrowLeft,
   ArrowRigth: ArrowRigth,
@@ -48,11 +54,15 @@ export const iconList = {
   CheckboxUnchecked: CheckboxUnchecked,
   Close: Close,
   Exclamation: Exclamation,
+  Hamburger: Hamburger,
   HelpCircle: HelpCircle,
   Lock: Lock,
   Pencil: Pencil,
+  Question: Question,
   RadioChecked: RadioChecked,
   RadioUnchecked: RadioUnchecked,
+  Suitcase: Suitcase,
+  SuitcaseArrow: SuitcaseArrow,
   VisibilityOff: VisibilityOff,
   VisibilityOn: VisibilityOn
 }
