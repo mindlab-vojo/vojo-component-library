ca5454ee21a95cde4025ac81ddde7ad6eb04aeb0
Leonardo Isso <leoisso.work@gmail.com>
2020-08-27 08:55:25 -0300

    Update stories wiuth new variables

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions'
 
 import Header from '.';
 import categories from '../../../.storybook/categories';
 import { ThemeProvider } from "../../../src";
+import { menuButtons, menuItems, termsObject } from '../SideMenu/SideMenu.stories';
 
 export const header = () => {
-  return <Header/>
+  const buttons = object('menuButtons', menuButtons)
+  const items = object('menuItems', menuItems)
+  const onClickLogo = action('onClickLogo')
+  const tems = object('menuTems', termsObject)
+
+  return (
+    <Header
+      menuButtons={buttons}
+      menuItems={items}
+      onClickLogo={onClickLogo}
+      menuTems={tems}
+    />
+  )
 }
 
 header.story = {
