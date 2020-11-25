d0da26bc59e00955202c79ff4348e0ae785dae34
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-11-25 14:43:49 -0300

    feat: add tab selection method

--- src/components/TabSelector/TabSelector.js
+++ src/components/TabSelector/TabSelector.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ThemeConsumer from '../../style/ThemeManager/ThemeProvider'
 import * as TabSelector from './TabSelector.style'
 
 const TabSelectorComponent = () => {
-  const tabs = [{ title: 'Dados de Login', active: true }, { title: 'Dados Pessoais', active: false }]
+  const [ tabs, setTabs ] = useState([{ 
+    title: 'Dados de Login', 
+    active: true 
+  }, { 
+    title: 'Dados Pessoais', 
+    active: false 
+  }])
+
+  const setThisTabAsActive = index => {
+    const newTabsState = []
+
+    tabs.forEach((tab, key) => {
+      key === index ? newTabsState.push({ ...tab, active: true }) : newTabsState.push({ ...tab, active: false }) 
+    })
+
+    setTabs(newTabsState)
+  }
 
   const renderComponent = theme => {
     return (
@@ -11,9 +27,17 @@ const TabSelectorComponent = () => {
         {
           tabs.map((tab, index) => {
             return tab.active ?
-              <TabSelector.ActiveTab theme={ theme } key={ index }>{ tab.title }</TabSelector.ActiveTab>
+              <TabSelector.ActiveTab 
+                theme={ theme } 
+                key={ index }
+                onClick={ () => setThisTabAsActive(index) }
+              >{ tab.title }</TabSelector.ActiveTab>
               :
-              <TabSelector.InactiveTab theme={ theme } key={ index }>{ tab.title }</TabSelector.InactiveTab>
+              <TabSelector.InactiveTab 
+                theme={ theme } 
+                key={ index }
+                onClick={ () => setThisTabAsActive(index) }
+              >{ tab.title }</TabSelector.InactiveTab>
           })
         }
       </TabSelector.Wrapper>
