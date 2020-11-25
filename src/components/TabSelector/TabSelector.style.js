3d4c48025253ed0d3588952afff3976b8e4ef964
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-11-25 14:11:07 -0300

    style: add active and inactive tab styled components

--- src/components/TabSelector/TabSelector.style.js
+++ src/components/TabSelector/TabSelector.style.js
@@ -6,6 +6,23 @@ export const Wrapper = styled.ul`
   align-items: center;
   border: 1px solid ${props => props.theme.colors.lightBorderColor};
   border-radius: 6px;
-  padding: 10px 20px;
+  padding: 6px;
   width: 100%;
+  list-style: none;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+`
+
+export const ActiveTab = styled.li`
+  background-color: ${props => props.theme.colors.lightPurpleColor};
+  color: ${props => props.theme.colors.primaryColor};
+  padding: 15px 20px;
+  flex: 1 0 0;
+`
+
+export const InactiveTab = styled.li`
+  color: ${props => props.theme.colors.textMutedColor};
+  flex: 1 0 0;
 `
