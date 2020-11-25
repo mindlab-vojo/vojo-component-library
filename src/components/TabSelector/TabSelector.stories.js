e62b5ad0c331bedd3fd970864cbde5aeee26f320
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-11-25 15:13:18 -0300

    feat: add knobs and all1 to TabSelector storybook

--- src/components/TabSelector/TabSelector.stories.js
+++ src/components/TabSelector/TabSelector.stories.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import categories from '../../../.storybook/categories'
 import TabSelector from './TabSelector'
+import { withKnobs, object } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 import { ThemeProvider } from "../../../src";
 import Container from '../Container';
 
 export const tabselector = () => {
+  const tabs = object('Tabs', [{ title: 'Dados de Login', active: true }, { title: 'Dados Pessoais', active: false }])
+
   return (
     <Container maxWidth="desktop">
-      <TabSelector />
+      <TabSelector 
+        tabs={ tabs }
+      />
     </Container>
   )
 }
@@ -15,7 +21,7 @@ export const tabselector = () => {
 export default {
   title: categories.Tabs,
   component: tabselector,
-  decorators: [story => <ThemeProvider>{ story() }</ThemeProvider>]
+  decorators: [withKnobs, withA11y, story => <ThemeProvider>{ story() }</ThemeProvider>]
 }
 
 tabselector.story = {
