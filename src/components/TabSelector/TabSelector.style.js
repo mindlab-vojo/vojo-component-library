774bcf5678f06a73641dfd73b34c8f01e912ded0
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-11-25 13:48:43 -0300

    refactor: change TabSelector wrapper to a ul element

--- src/components/TabSelector/TabSelector.style.js
+++ src/components/TabSelector/TabSelector.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.ul`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
