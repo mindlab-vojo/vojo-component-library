6959321b2ce8a5843452364fb66fa359d0a264d7
AlvaroCarvalho <alvaro.azeredo@hotmail.com>
2020-11-25 13:46:40 -0300

    style: add TabSelector wrapper styled component

---
+++ src/components/TabSelector/TabSelector.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid ${props => props.theme.colors.lightBorderColor};
+  border-radius: 6px;
+  padding: 10px 20px;
+  width: 100%;
+`
